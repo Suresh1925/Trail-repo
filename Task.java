23ae98e7e4189404697e6582811f5b2efbb7c662
dnethaji <DNethaji@its.jnj.com>
2023-12-20 17:35:42 +0530

    java file modified

--- Task.java
+++ Task.java
@@ -1,1 +1,11 @@
-"Java Task"
+public class Task {
+public static void main(String[]args){
+int year=2023;
+if(year%4==0){
+System.out.println("It is a LeapYear");
+}
+else{
+System.out.println("It is not a LeapYear");
+}
+}
+}
